723d447df1bd828698c4b84084356b61a0c50aaa
Anthony Shaga <antonyshaga@gmail.com>
2025-02-24 21:50:36 +0200

    first commit

--- nestjs-server/src/libs/common/utils/is-dev.util.ts
+++ nestjs-server/src/libs/common/utils/is-dev.util.ts
@@ -3,8 +3,7 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const isDev = (configService: ConfigService) => {
+export const isDev = (configService: ConfigService) =>
 	configService.getOrThrow('NODE_ENV') === 'development'
-}
 
 export const IS_DEV_ENV = process.env.NODE_ENV === 'development'
