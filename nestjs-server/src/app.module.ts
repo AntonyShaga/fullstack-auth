cc77d73b3283ec4f821e6dbe80fbf8afb13b5750
Anthony Shaga <antonyshaga@gmail.com>
2025-02-25 21:37:02 +0200

    email password

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { EmailConfirmationModule } from '@/auth/email-confirmation/email-confirmation.module'
+import { PasswordRecoveryModule } from '@/auth/password-recovery/password-recovery.module'
 import { ProviderModule } from '@/auth/provider/provider.module'
 import { IS_DEV_ENV } from '@/libs/common/utils/is-dev.util'
+import { MailModule } from '@/libs/mail/mail.module'
 
 import { AuthModule } from './auth/auth.module'
 import { PrismaModule } from './prisma/prisma.module'
@@ -17,7 +20,10 @@ import { UserModule } from './user/user.module'
 		PrismaModule,
 		AuthModule,
 		UserModule,
-		ProviderModule
+		ProviderModule,
+		MailModule,
+		EmailConfirmationModule,
+		PasswordRecoveryModule
 	]
 })
 export class AppModule {}
